f01cd26df2a03ff23528cb7fa7977b9903766dc8
Costin <vldcostin@gmail.com>
2025-05-15 22:41:40 +0300

    General fixes

--- src/main/java/com/mobylab/springbackend/service/AuthService.java
+++ src/main/java/com/mobylab/springbackend/service/AuthService.java
@@ -14,7 +14,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -24,8 +23,6 @@ import java.util.UUID;
 public class AuthService {
     @Autowired
     private RequestService requestService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -36,9 +33,6 @@ public class AuthService {
     private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
 
     public UUID registerWithoutFamilyId(RegisterRequestDto registerRequestDto) {
-        String password = registerRequestDto.getPassword();
-        registerRequestDto.setPassword(passwordEncoder.encode(password));
-
         ResponseEntity<UUID> response = requestService.sendPostRequest(
                 "/authproxy/register",
                 Collections.emptyMap(),
@@ -54,9 +48,6 @@ public class AuthService {
     }
 
     public void registerWithFamilyId(RegisterWithFamilyIdRequestDto registerRequestDto) {
-        String password = registerRequestDto.getPassword();
-        registerRequestDto.setPassword(passwordEncoder.encode(password));
-
         ResponseEntity<Void> response = requestService.sendPostRequest(
                 "/authproxy/register/family-id",
                 Collections.emptyMap(),
@@ -70,9 +61,6 @@ public class AuthService {
     }
 
     public String login(LoginRequestDto loginRequestDto) {
-        String password = loginRequestDto.getPassword();
-        loginRequestDto.setPassword(passwordEncoder.encode(password));
-
         ResponseEntity<Void> response = requestService.sendPostRequest(
                 "/authproxy/login",
                 Collections.emptyMap(),
@@ -84,10 +72,10 @@ public class AuthService {
             throw new AuthException("Login request failed.");
         }
 
-        Authentication authentication = authenticationManager.authenticate(
+        Authentication authentication =
                 new UsernamePasswordAuthenticationToken(
                         loginRequestDto.getEmail(),
-                        loginRequestDto.getPassword()));
+                        loginRequestDto.getPassword());
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return jwtGenerator.generateToken(authentication);
